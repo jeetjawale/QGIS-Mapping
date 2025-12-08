647b4c8591a0b7f4a64446bfa2677bfa484556e0
Jeet <jeetjawale@zohomail.in>
2025-12-08 14:13:20 +0530

    Add Firebase and configuration files with environment variables

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const MAPTILER_KEY = import.meta.env.VITE_MAPTILER_KEY;
+export const OPENROUTESERVICE_KEY = import.meta.env.VITE_OPENROUTESERVICE_KEY;
+export const OPENWEATHERMAP_KEY = import.meta.env.VITE_OPENWEATHERMAP_KEY;
